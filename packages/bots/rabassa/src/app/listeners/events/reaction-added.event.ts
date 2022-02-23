092c4491cda77fe72e9eac4925dcf4debed485fd
Michael Schilling <mschilling@arkid.nl>
2022-02-23 21:05:23 +0000

    add emoji util function

--- packages/bots/rabassa/src/app/listeners/events/reaction-added.event.ts
+++ packages/bots/rabassa/src/app/listeners/events/reaction-added.event.ts
@@ -1,10 +1,10 @@
-import { reverseText } from '../../utils';
+import { emoji, reverseText } from '../../utils';
 
 export const onReactionAdded = async ({ event, client, logger }) => {
   try {
     const result = await client.chat.postMessage({
       channel: event.item.channel,
-      text: `That's right, <@${event.user}>! ${event.reaction}`,
+      text: `That's right, <@${event.user}>! ${emoji(event.reaction)}`,
       thread_ts: event.item.ts,
     });
     logger.info(result);
--- packages/bots/rabassa/src/app/utils/index.ts
+++ packages/bots/rabassa/src/app/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './reverse-text.util';
+export * from './slack-emoji.util';
