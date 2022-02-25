32eefbd38a08478cde4e5a740284938834a9cf42
Michael Schilling <mschilling@arkid.nl>
2022-02-25 15:54:34 +0000

    add support for french

---
+++ packages/bots/rabassa/src/app/utils/translate.util.ts
@@ -0,0 +1,15 @@
+import lib from '@google-cloud/translate';
+
+const projectId = process.env['PROJECT_ID'];
+
+// Instantiates a client
+const translateClient = new lib.v2.Translate({ projectId });
+
+export const translate = async (text, target) => {
+  if (!(text && target)) {
+    return null;
+  }
+
+  const [translation] = await translateClient.translate(text, target);
+  return translation;
+};
