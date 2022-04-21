6c8f37cad14cbd1fbfb3f45314609d0d8877dfeb
Michael <mschilling@users.noreply.github.com>
2022-04-21 21:18:26 +0200

    feat: add deploy support + small improvements (#5)

--- packages/bots/rabassa/src/app/listeners/events/app-mention.event.ts
+++ packages/bots/rabassa/src/app/listeners/events/app-mention.event.ts
@@ -1,24 +1,16 @@
-export const onAppMentionEvent = async ({
-  event,
-  body,
-  say,
-  client,
-  logger,
-}) => {
+export const onAppMentionEvent = async ({ event, client, say, logger }) => {
   try {
-    // console.log(body);
-
-    const result = await client.reactions.add({
+    await client.reactions.add({
       name: '+1',
       channel: event.channel,
       timestamp: event.ts,
     });
 
-    // await say({
-    //   // as_user: true,
-    //   text: `hi <@${event.user}>`,
-    //   thread_ts: event.thread_ts ?? event.ts,
-    // });
+    await say({
+      // as_user: true,
+      text: `hi <@${event.user}>, what's up?`,
+      thread_ts: event.thread_ts ?? event.ts,
+    });
   } catch (error) {
     logger.error(error);
   }
