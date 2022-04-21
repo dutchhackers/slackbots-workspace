758b4e89290171d0a359a4c5700e25392297457e
Michael Schilling <mschilling@move4mobile.com>
2022-04-21 21:09:33 +0200

    chore: code improvemets

--- packages/bots/rabassa/src/app/bots-rabassa.ts
+++ packages/bots/rabassa/src/app/bots-rabassa.ts
@@ -2,13 +2,15 @@ import { App } from '@slack/bolt';
 import { register } from './listeners';
 
 async function bootstrap() {
-  const app = new App({
+  const appOptions = {
     token: process.env['SLACK_BOT_TOKEN'],
     signingSecret: process.env['SLACK_SIGNING_SECRET'],
     socketMode: true,
     appToken: process.env['SLACK_APP_TOKEN'],
     port: +process.env['PORT']! || 3000,
-  });
+  };
+
+  const app = new App(appOptions);
 
   register(app);
 
